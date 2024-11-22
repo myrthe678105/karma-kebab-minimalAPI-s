ccf0ad93154966cb4364c743a7cacce93b1fc498
Betül Beril (Beth) <90333992+bberildundar@users.noreply.github.com>
2024-11-22 14:46:14 +0100

    dockerfile

--- DutyAPI/DutyAPI/obj/Debug/net8.0/DutyAPI.AssemblyInfo.cs
+++ DutyAPI/DutyAPI/obj/Debug/net8.0/DutyAPI.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("02091fc9-f0ff-4f91-a2d1-c993ebbcd41e")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DutyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b59b75cf4250fdb787b5ff1a83076ca00b134837")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d5af9a0241ef3f433b76534bc715eea93b3f6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DutyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DutyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
